2c9b9818609e6e96056023c1a372086f15a581e3
simon <kennychen614@hotmail.com>
2016-08-18 16:49:41 +0200

    merge

--- practice/generator.py
+++ practice/generator.py
@@ -13,5 +13,29 @@ def fibonacci():
 
 f = fibonacci()
 for i in range(10):
-    print(f.next())
+    print(next(f))
+
+# generator-iterator example
+# return and receive data
+
+
+def gen1():
+    a = 0
+    b = 0
+    while(True):
+        b = (yield a)
+        print("receive: " + b)
+        a = b + 'pick'
+
+f = gen1()
+f.send(None)
+s = "return: "
+print(s + f.send('1'))
+print(s + f.send('2'))
+print(s + f.send('10'))
+
+# generator expression
+g = (i*2 for i in range(10))
+for v in g:
+    print(v)
 
